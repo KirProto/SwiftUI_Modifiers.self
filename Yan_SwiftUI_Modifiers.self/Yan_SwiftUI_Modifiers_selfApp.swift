cada98ce58bf963170c0bc6e85399300d8bb714f
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 08:52:58 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Modifiers.self/Yan_SwiftUI_Modifiers_selfApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Modifiers_selfApp.swift
+//  Yan_SwiftUI_Modifiers.self
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Modifiers_selfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
